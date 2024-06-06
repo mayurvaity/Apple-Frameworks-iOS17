a7e161ceca704ddec660aef8f72dacdd4025880c
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-06 22:23:37 +0530

    Initial Commit

---
+++ Apple-Frameworks-iOS17/Apple_Frameworks_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_iOS17App.swift
+//  Apple-Frameworks-iOS17
+//
+//  Created by Mayur Vaity on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
